6cab628dccdaf74bc7af16d03e9eeee87c0870b6
Kiddibig <s223759@dtu.dk>
2025-05-01 13:54:53 +0200

    Add files via upload

---
+++ Projekt 2 - machineProg/main.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+#define length 10
+#define width 10
+#define NUM_TABLEAU_PILES 7
+#define NUM_TABLEAU_ROWS 10
+#define NUM_CARDS 52
+
+
+
+
+int main(void) {
+    //////////////////
+
+    for (int i = 0; i < NUM_TABLEAU_PILES; i++) {
+        printf("C%d   ", i + 1);
+    }
+    printf("\n");
+    int j = 0;
+    int line = 0;
+    int lineline = 1;
+    while (j < NUM_CARDS) {
+        for (int k = 0; k < NUM_TABLEAU_PILES; k++) {
+            printf("[]");
+            printf("   ");
+            j++;
+            if (j == NUM_CARDS) {
+                break;
+            }
+
+        }
+        line++;
+        if (line == 0 || line % 2) {
+            printf("\t");
+            printf("\t");
+            printf("[]");
+            printf("\t");
+            printf("F%d",lineline);
+            lineline++;
+        }
+
+        printf("\n");
+    }
+    printf("LAST Command:\n"); //remember to add link to last command
+    printf("Message:\n");//remember to add link to message
+    printf("INPUT >");
+
+    //////////////////
+
+}
+
+
+
